7888e0f3720aac5cbfa7eaf657c8fb6e4011bcd3
Mondal10 <amitmondal111.am@gmail.com>
2019-01-06 17:28:34 +0530

    To do list with dummy(local) data created

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -1,13 +1,40 @@
 //Used to manipulate the data(get/post/delete) and Handle the route
+var bodyParser = require('body-parser');
+
+var urlEncodedParser = bodyParser.urlencoded({
+    extended: false
+});
+
+var data = [{
+    item: 'get milk'
+}, {
+    item: 'play fifa'
+}, {
+    item: 'sleep'
+}, {
+    item: 'Gym'
+}];
+
+
 
 module.exports = function (app) {
 
     app.get('/todo', function (req, res) {
-        res.render('todo');
+        res.render('todo', {
+            todos: data
+        });
     });
 
-    app.post('/todo', function (req, res) {});
+    app.post('/todo', urlEncodedParser, function (req, res) {
+        data.push(req.body);
+        res.json(data);
+    });
 
-    app.delete('/todo', function (req, res) {});
+    app.delete('/todo/:item', function (req, res) {
+        data = data.filter(function (todo) {
+            return todo.item.replace(/ /g, '-') !== req.param.item;
+        });
+        res.json(data)
+    });
 
 }
