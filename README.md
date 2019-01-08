2302516ff725b01c7b7bcd7f9e5efe5245844116
Mondal10 <amitmondal111.am@gmail.com>
2019-01-08 17:39:56 +0530

    Connectivity with MongoDB

--- README.md
+++ README.md
@@ -6,5 +6,8 @@
 2. Install dependencies and saving it in  ```package.json```
    - <b>[Express:](https://www.npmjs.com/package/express)</b> use ```$ npm install express -save```  
    - <b>[EJS:](https://www.npmjs.com/package/ejs)</b> use ```$ npm install ejs -save```
-   - <b>[Body-Parser:](https://www.npmjs.com/package/body-parser)</b> ```$ npm install body-parser -save```
-3. 
+   - <b>[Body-Parser:](https://www.npmjs.com/package/body-parser)</b> use ```$ npm install body-parser -save```
+   - <b>[Mongoose](https://www.npmjs.com/package/mongoose)</b> use ```$ npm install mongoose -save```
+   - <b>[UUID](https://www.npmjs.com/package/uuid)</b> use ```npm install uuid```
+   #### Optional 
+   - <b>[Nodemon](https://www.npmjs.com/package/nodemon)</b> use ```npm install -g nodemon``` Helps to automatically restart the node application when file changes in the directory are detected.
--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -1,44 +1,55 @@
 //Used to manipulate the data(get/post/delete) and Handle the route
 var bodyParser = require('body-parser');
-var uuid = require('uuid');
+var uuid = require('uuid/v1');
+var mongoose = require('mongoose');
+
+//Connect to the database of mlab
+mongoose.connect('mongodb://test123456:test123456@ds127978.mlab.com:27978/to-do-app', { useNewUrlParser: true });
+
+//Create a database schema
+var todoSchema = new mongoose.Schema({
+    id: String,
+    item: String
+});
+
+//Create new model Todo
+var Todo = mongoose.model('Todo', todoSchema);
+// var item1 = Todo({id: uuid(), item: 'go gym'}).save(function(err){
+//     if(err) throw err;
+//     console.log('Item saved');
+// });
+
 var urlEncodedParser = bodyParser.urlencoded({
     extended: false
 });
 
-var data = [{
-    id: uuid(),
-    item: 'get milk'
-}, {
-    id: uuid(),
-    item: 'play fifa'
-}, {
-    id: uuid(),
-    item: 'sleep'
-}, {
-    id: uuid(),
-    item: 'Gym'
-}];
-
-
-
 module.exports = function (app) {
 
     app.get('/todo', function (req, res) {
-        res.render('todo', {
-            todos: data
+        //Get data from mongoDB and pass it to view
+        Todo.find({}, function(err,data){
+            if(err) throw err;
+            res.render('todo', {
+                todos: data
+            });
         });
+        
     });
 
     app.post('/todo', urlEncodedParser, function (req, res) {
-        data.push({id: uuid(),item: req.body.item});
-        res.json(data);
+        //Get data from the view and add to mongoDB
+        var newTodo = Todo({id: uuid(),item: req.body.item}).save(function(err,data){
+            if(err) throw err;
+            res.json(data);
+        });
     });
 
     app.delete('/todo/:item', function (req, res) {
-        data = data.filter(function (todo) {
-            return todo.id !== req.params.item;
-        });
-        res.json(data)
+        //Delete the requested/clicked item(in view) from mongoDB
+        Todo.find({id: req.params.item}).remove(function(err,data){
+            if(err) throw err;
+            res.json(data)
+        }); 
     });
 
 }
